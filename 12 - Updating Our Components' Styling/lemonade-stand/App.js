12253a4f943a578e9630495a07f103b995d786bb
KennyWorth <93877201+KennyWorth@users.noreply.github.com>
2022-10-03 16:45:18 -0500

    Updating Styling changes added

---
+++ 12 - Updating Our Components' Styling/lemonade-stand/App.js
@@ -0,0 +1,9 @@
+import Home from './Screens/Customer/Home';
+const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+)
+
+
+export default App;
---
+++ 12 - Updating Our Components' Styling/lemonade-stand/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: ${({ w }) => w};
+    height: ${({ h }) => h};
+    border: 1px solid black;
+    background: ${({ bg }) => bg};
+    border-radius: 3px;
+    color: #fff;
+    font-size: 20px;
+    &:disabled {
+        background: #ccc;
+        color: #555;
+    }
+`
+export default Button
